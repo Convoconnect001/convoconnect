a10b4298a6c8d09c4b8cdd8639441ce84621d5f0
Convoconnect001 <convoconnect2025@gmail.com>
2026-04-04 16:41:36 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div>
+        <img src="/logo.png" alt="ConvoConnect Logo" srcset="" height={60} />
+        <div className="foot-brand">ConvoConnect</div>
+        <p className="foot-tagline">
+          Connecting through conversations
+        </p>
+      </div>
+
+      <p className="foot-copy">© 2026 ConvoConnect. All rights reserved.</p>
+    </footer>
+  );
+}
